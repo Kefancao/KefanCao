887aa3a85dafc409fb7f76814a84075eecd650b4
Kefancao <76069770+Kefancao@users.noreply.github.com>
2021-01-14 15:23:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function togglePopup(){
+    document.getElementById("about-button").classList.toggle("active");
+}
+
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove', (e) => {
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+})
